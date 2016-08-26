b527223a698ceb11bd60e46dd5de801ead86022c
Alperen Guman <guman@lperen.com>
2016-08-25 23:24:56 -0700

    map coordinate tracking logic created

--- map.py
+++ map.py
@@ -5,20 +5,6 @@ class Map:
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
-        if 'size' not in kwargs:
-            self.setup_map()
-            self.print_map()
-        else:
-           pass
-
-    def setup_map(self):
-        map_size = input("Select map size [S]mall [M]edium or [L]arge]")
-        if map_size.lower()== 's':
-            self.size=5
-        elif map_size.lower()== 'm':
-            self.size=10
-        elif map_size.lower()== 'l':
-            self.size=15
 
     def print_map(self):
         columns_letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
@@ -28,19 +14,44 @@ class Map:
         rows = [n for n in numbers]
         coordinates = []
 
-        for x in columns:
-            for y in rows:
-                coordinates.append((x, y))
+        for numbers in rows:
+            for letters in columns:
+                coordinates.append((letters, numbers))
+
+        coordinates_dict = {}
+        for items in coordinates:
+            coordinates_dict[items]="EMPTY"
+
+        coordinates_dict[('c',2)] = "FULL"
+
+        map_display = []
+        n = 0
+        m = 0
+        map_display.append(' '+str(rows[m]))
+        m = 1
+        for item in coordinates:
+            n = n+1
+            if coordinates_dict[item]=="EMPTY" and n%len(columns) == 0 and m<len(rows):
+                map_display.append('0')
+                map_display.append('\n')
+                if rows[m]<10:
+                    map_display.append(' '+str(rows[m]))
+                else:
+                    map_display.append(str(rows[m]))
+                m = m+1
+            elif coordinates_dict[item]=="EMPTY" and n%len(columns) == 0:
+                map_display.append('0')
+                map_display.append('\n')
+            elif coordinates_dict[item]=="EMPTY":
+                map_display.append('0')
+            else:
+                map_display.append('1')
+        print('\n','  ', ' '.join(columns).upper())
+        print('',' '.join(map_display))
 
 
+
+        print(coordinates_dict)
         print(rows)
         print(columns)
-        print(coordinates)
 
-        print("\n")
-        n = 0
-        row_number = 0
-        while n != self.size:
-            print(row_number, "0 "*self.size)
-            row_number += 1
-            n = n + 1
